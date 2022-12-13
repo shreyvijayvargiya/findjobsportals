42fd9000445ab52b925394096a53b503918caebd
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2022-12-13 15:40:03 +0530

    add jobs portals typeform added

--- Components/Home/index.jsx
+++ Components/Home/index.jsx
@@ -36,22 +36,32 @@ const Home = () => {
 
 	return (
 		<div>
-			<div className="md:w-3/5 xl:w-3/5 2xl:w-2/5 mx-auto xxs:w-full xs:w-full sm:w-full m-10">
-				<div className="flex justify-start gap-2 items-start">
-					<div>
+			<div className="fixed left-0 right-0 top-0 p-4 bg-gray-50">
+				<div className="flex justify-between items-center">
+					<div className="flex justify-start items-center gap-2">
 						<img
 							src="/Log Book Thumbnail.png"
-							className="w-24 h-24 mx-auto rounded-md"
+							className="w-10 h-10 mx-auto rounded-md"
 						/>
+						<p className="text-md font-bold">Find Jobs Portals</p>
 					</div>
-					<div>
-						<p className="text-4xl font-bold">Jobs Portals around the world</p>
-						<p>
-							Simplify your job search with extensive list of jobs portals
-							around the world
-						</p>
-					</div>
+					<button
+						data-tf-popup="Z7vghLtB"
+						data-tf-iframe-props="title=FindJobsPortals-Adding-Portals-Form"
+						data-tf-medium="snippet"
+						className="border bg-indigo-600 hover:bg-indigo-700 rounded-md p-2 text-gray-100"
+					>
+						Add Portals
+					</button>
+					<script src="//embed.typeform.com/next/embed.js"></script>
 				</div>
+			</div>
+			<div className="md:w-3/5 xl:w-3/5 2xl:w-2/5 mx-auto xxs:w-full xs:w-full sm:w-full my-20">
+				<p className="text-4xl font-bold">Jobs Portals around the world</p>
+				<p>
+					Simplify your job search with extensive list of jobs portals around
+					the world
+				</p>
 				<div className="my-6">
 					<Table withBorder withColumnBorders>
 						<thead>{ths}</thead>
@@ -117,59 +127,61 @@ const Home = () => {
 					</a>
 				</p>
 				<hr />
-				<p className="text-sm text-gray-400 my-1 p-2 font-bold text-center">Social links</p>
+				<p className="text-sm text-gray-400 my-1 p-2 font-bold text-center">
+					Social links
+				</p>
 				<div className="flex justify-center items-center m-1 gap-4">
 					<Tooltip label="Twitter">
-							<a
-								href="https://twitter.com/treyvijay"
-								target="_blank"
+						<a
+							href="https://twitter.com/treyvijay"
+							target="_blank"
+							data-tip="Twitter"
+							alt="Twitter"
+							aria-label="Twitter"
+							rel="noreferrer"
+						>
+							<AiFillTwitterCircle
+								size={24}
+								color={colors.blue[500]}
 								data-tip="Twitter"
-								alt="Twitter"
-								aria-label="Twitter"
-								rel="noreferrer"
-							>
-								<AiFillTwitterCircle
-									size={24}
-									color={colors.blue[500]}
-									data-tip="Twitter"
-								/>
-							</a>
+							/>
+						</a>
 					</Tooltip>
 					<Tooltip label="Github">
-							<a
-								href="https://github.com/shreyvijayvargiya"
-								target="_blank"
-								rel="noreferrer"
-							>
-								<AiFillGithub size={24} color={colors.indigo[500]} />
-							</a>
+						<a
+							href="https://github.com/shreyvijayvargiya"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<AiFillGithub size={24} color={colors.indigo[500]} />
+						</a>
 					</Tooltip>
 					<Tooltip label="Medium">
-							<a
-								href="https://medium.com/@shreyvijayvargiya26"
-								target="_blank"
-								rel="noreferrer"
-							>
-								<AiFillMediumCircle size={24} color={colors.orange[500]} />
-							</a>
+						<a
+							href="https://medium.com/@shreyvijayvargiya26"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<AiFillMediumCircle size={24} color={colors.orange[500]} />
+						</a>
 					</Tooltip>
 					<Tooltip label="Devto">
-							<a
-								href="https://dev.to/shreyvijayvargiya"
-								target="_blank"
-								rel="noreferrer"
-							>
-								<FaDev size={24} color={colors.pink[500]} />
-							</a>
+						<a
+							href="https://dev.to/shreyvijayvargiya"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FaDev size={24} color={colors.pink[500]} />
+						</a>
 					</Tooltip>
 					<Tooltip label="Youtube">
-							<a
-								href="https://www.youtube.com/channel/UC6I-Tz6QwYbJpoIK7l0NtXA"
-								target="_blank"
-								rel="noreferrer"
-							>
-								<AiFillYoutube size={24} color={colors.red[500]} />
-							</a>
+						<a
+							href="https://www.youtube.com/channel/UC6I-Tz6QwYbJpoIK7l0NtXA"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<AiFillYoutube size={24} color={colors.red[500]} />
+						</a>
 					</Tooltip>
 				</div>
 			</div>
