b312d17cb9a33e58346d735c4255ce6c54963661
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2022-12-17 14:09:13 +0530

    Table UI improved

--- Components/Home/index.jsx
+++ Components/Home/index.jsx
@@ -27,61 +27,72 @@ const Home = () => {
 	}, []);
 
 	const ths = (
-		<tr className="bg-gray-50 text-left">
-			<th>Name</th>
-			<th>Category</th>
-			<th>Website</th>
+		<tr className="bg-black text-left">
+			<th className="text-white p-4">Name</th>
+			<th className="text-white p-4">Website</th>
+			<th className="text-white p-4">Category</th>
 		</tr>
 	);
 
 	const getBadgeColor = (type) => {
 		let color;
-		if(type === "All"){
-			color = "teal"
-		}else if(type === "Development"){
-			color = "orange"
-		}else if(type === "Designing"){
-			color = "voilet"
-		}else if(type === "Blockchain"){
-			color= "pink"
+		if (type === "All") {
+			color = colors.green[400];
+		} else if (type === "Development") {
+			color = colors.orange[400];
+		} else if (type === "Designing") {
+			color = colors.indigo[400];
+		} else if (type === "Blockchain") {
+			color = colors.pink[400];
 		}
 		return color;
-	}
+	};
 
 	return (
 		<div>
-			<div className="md:w-3/5 xl:w-3/5 2xl:w-2/5 mx-auto xxs:w-full xs:w-full sm:w-full my-20">
+			<div className="md:w-3/5 xl:w-4/6 2xl:w-2/5 mx-auto xxs:w-full xs:w-full sm:w-full my-20">
 				<p className="text-4xl font-bold">Jobs Portals around the world</p>
 				<p>
 					Simplify your job search with extensive list of jobs portals around
 					the world
 				</p>
 				<div className="my-6">
-					<Table withBorder withColumnBorders>
+					<table className="border-2 border-black rounded-md w-full">
 						<thead>{ths}</thead>
 						<tbody>
 							{data &&
 								data.map((item) => (
-									<tr key={item.name} className="hover:bg-gray-100 ">
-										<td className="text-2xl font-regular">{item.name}</td>
-										<td>
-											{item.domain.map((element) => (
-												<Badge color={getBadgeColor(element)}>{element}</Badge>
-											))}
+									<tr
+										key={item.name}
+										className="hover:bg-gray-200 rounded-md border-2 border-black"
+									>
+										<td className="text-md font-regular p-3 border-r-2 border-black">
+											{item.name}
 										</td>
-										<td>
+
+										<td className="p-3 hover:text-white border-r-2 border-black">
 											<a
 												href={item.website}
 												target="_blank"
-												className="text-pink-800 hover:text-pink-700 hover:underline"
+												className="text-black underline hover:font-semibold"
 											>
 												{item.website}
 											</a>
 										</td>
+										<td className="p-3 border-r-2 border-black">
+											{item.domain.map((element) => (
+												<span
+													className="rounded-md py-2 px-4 m-2 text-xs font-semibold"
+													style={{ backgroundColor: getBadgeColor(element) }}
+												>
+													{element}
+												</span>
+											))}
+										</td>
 									</tr>
 								))}
 						</tbody>
-					</Table>
+					</table>
 				</div>
 			</div>
 		</div>
